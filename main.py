111cebeb7e4a76a00ae513379c89ba2d05998e33
Jbirch99 <jack.birch99@outlook.com>
2021-10-01 10:34:17 +0100

    Made a small change to the main.py file. Each time a request is sent pandas no longer has to open the excel file each time. Instead the file is simply opened once.

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ import requests
 app = Flask(__name__)
 api = Api(app)
 
+#Import the region data into a data frame to be manipulated
+df_region_1 = pd.read_excel("ONSByRegion2019.xlsx", index_col = 1)
+df_region_2 = pd.read_excel("ONSByRegion2019.xlsx", index_col = 2)
+
 #Takes the input caseid and extracts the postcodes into a list
 def retrieve_postcode(caseid):
     postcode_list = []
@@ -49,34 +53,29 @@ def find_required_fields(region_list):
     for element in region_list:
         dict_outgoings = {}
 
-        #Import the region data into data frame to be manipulated
-        df_region = pd.read_excel("ONSByRegion2019.xlsx", index_col = 1)
-
         #For the clients lookup there different qualities 
-        dict_outgoings["outgoings_communications:"] = str(df_region.loc["Communication", element])
-        dict_outgoings["outgoings_food:"] = str(df_region.loc["Food", element])
-        dict_outgoings["outgoings_mortgage_rent:"] = str(df_region.loc["Actual rentals for housing", element])
-        dict_outgoings["outgoings_mortgage_insurance:"] = str(df_region.loc["Insurance", element])
-        dict_outgoings["outgoings_mortgage_investments:"] = str(df_region.loc["Savings and investments", element])
-        dict_outgoings["outgoings_council_tax:"] = str(df_region.loc["council tax etc.", element])      #As row index "Housing: mortgage interest payments council tax etc." went over two lines only accepted second line as row index
-        dict_outgoings["outgoings_clothing:"] = str(df_region.loc["Clothing", element])
-        dict_outgoings["outgoings_water:"] = str(df_region.loc["relating to the dwelling", element])
-        dict_outgoings["outgoings__other_living_costs:"] = str(df_region.loc["Electricity, gas and other fuels", element])
-        dict_outgoings["outgoings_entertainment:"] = str(df_region.loc["processing equipment", element])
-        dict_outgoings["outgoings_holidays:"] = str(df_region.loc["Package holidays", element])
-        dict_outgoings["outgoings_pension"] = str(df_region.loc["Life assurance, contributions to pension funds", element])
-        dict_outgoings["outgoings_car_costs"] = str(df_region.loc["Purchase of vehicles", element])
-        dict_outgoings["outgoings_other_transport_costs"] = str(df_region.loc["Transport services", element])
-        dict_outgoings["outgoings_car_costs"] = str(df_region.loc["Purchase of vehicles", element])
+        dict_outgoings["outgoings_communications:"] = str(df_region_1.loc["Communication", element])
+        dict_outgoings["outgoings_food:"] = str(df_region_1.loc["Food", element])
+        dict_outgoings["outgoings_mortgage_rent:"] = str(df_region_1.loc["Actual rentals for housing", element])
+        dict_outgoings["outgoings_mortgage_insurance:"] = str(df_region_1.loc["Insurance", element])
+        dict_outgoings["outgoings_mortgage_investments:"] = str(df_region_1.loc["Savings and investments", element])
+        dict_outgoings["outgoings_council_tax:"] = str(df_region_1.loc["council tax etc.", element])      #As row index "Housing: mortgage interest payments council tax etc." went over two lines only accepted second line as row index
+        dict_outgoings["outgoings_clothing:"] = str(df_region_1.loc["Clothing", element])
+        dict_outgoings["outgoings_water:"] = str(df_region_1.loc["relating to the dwelling", element])
+        dict_outgoings["outgoings__other_living_costs:"] = str(df_region_1.loc["Electricity, gas and other fuels", element])
+        dict_outgoings["outgoings_entertainment:"] = str(df_region_1.loc["processing equipment", element])
+        dict_outgoings["outgoings_holidays:"] = str(df_region_1.loc["Package holidays", element])
+        dict_outgoings["outgoings_pension"] = str(df_region_1.loc["Life assurance, contributions to pension funds", element])
+        dict_outgoings["outgoings_car_costs"] = str(df_region_1.loc["Purchase of vehicles", element])
+        dict_outgoings["outgoings_other_transport_costs"] = str(df_region_1.loc["Transport services", element])
+        dict_outgoings["outgoings_car_costs"] = str(df_region_1.loc["Purchase of vehicles", element])
         
-        #Have to change index column to retreive required data
-        df_region = pd.read_excel("ONSByRegion2019.xlsx", index_col = 2)
-        dict_outgoings["outgoings_sports:"] = str(df_region.loc["and equipment hire", element])
-        dict_outgoings["outgoings_child_care"] = str(df_region.loc["hire/repair of furniture/furnishings", element])
-        dict_outgoings["outgoings_fuel"] = str(df_region.loc["Petrol, diesel and other motor oils", element])
-        dict_outgoings["outgoings_ground_rent_service_charge_chared_equity_rent"] = str(df_region.loc["Second dwelling rent", element])
-        #dict_outgoings["outgoings_television_license"] = str(df_region.loc["and TV licences", element]) #This lookup didn't work
-        dict_outgoings["outgoings_household_repairs"] = str(df_region.loc["hire/repair of furniture/furnishings", element])
+        dict_outgoings["outgoings_sports:"] = str(df_region_2.loc["and equipment hire", element])
+        dict_outgoings["outgoings_child_care"] = str(df_region_2.loc["hire/repair of furniture/furnishings", element])
+        dict_outgoings["outgoings_fuel"] = str(df_region_2.loc["Petrol, diesel and other motor oils", element])
+        dict_outgoings["outgoings_ground_rent_service_charge_chared_equity_rent"] = str(df_region_2.loc["Second dwelling rent", element])
+        #dict_outgoings["outgoings_television_license"] = str(df_region_2.loc["and TV licences", element]) #This lookup didn't work
+        dict_outgoings["outgoings_household_repairs"] = str(df_region_2.loc["hire/repair of furniture/furnishings", element])
         
         outgoings_for_clients.append(dict_outgoings)
     
